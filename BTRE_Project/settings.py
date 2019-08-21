90ca0a689196b6506283755acd68cf41858fc13e
JakeTheReaper <jake.zerafa86@gmail.com>
2019-08-21 21:08:56 +1000

    Updated Admin / Added Models and View Methods

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'BTRE_Project/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
